2388393dd0e8cfaf2b7759dff141ad518a155028
Pariwat Yimpayoon <32978747+PariwatY@users.noreply.github.com>
2022-09-18 17:30:54 +0700

    add step deploy fire base

---
+++ portfolio-pek/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4pJ2A14nMQe4EJUs3x4rtk7_QErYNTHU",
+  authDomain: "portfolio-pek.firebaseapp.com",
+  projectId: "portfolio-pek",
+  storageBucket: "portfolio-pek.appspot.com",
+  messagingSenderId: "396020835790",
+  appId: "1:396020835790:web:d1ced0b448ba55eff9779f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
